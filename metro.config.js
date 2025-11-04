efe6e7aa32835115ce7e78cf0324ecf1fab2f7a5
Guilherme Helli Farinon Cordeiro <guilherme.cordeiro@valecard.com.br>
2025-11-04 00:18:40 -0300

    .

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,27 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
+// --- Adicionado ---
+// Obtenha a configuração padrão primeiro para acessar seu resolver
+const defaultConfig = getDefaultConfig(__dirname);
+const {
+  resolver: { sourceExts },
+} = defaultConfig;
+// --- Fim da Adição ---
+
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  // --- Adicionado ---
+  // Adicione 'cjs' à lista de extensões de arquivo que o Metro reconhece
+  resolver: {
+    sourceExts: [...sourceExts, 'cjs'],
+  },
+  // --- Fim da Adição ---
+};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Modificado para usar a 'defaultConfig' que definimos acima
+module.exports = mergeConfig(defaultConfig, config);
